23b68a0dbb3513ba5586c67ee40607be305bdccb
ObamaBolsa <daniel.alegria.lino@gmail.com>
2021-12-15 11:39:23 -0600

    Removed ignored files

---
+++ csharp_asp_practice_2/Controllers/HomeController.cs
@@ -0,0 +1,40 @@
+﻿using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using csharp_asp_practice_2.Models;
+
+namespace csharp_asp_practice_2.Controllers;
+
+public class HomeController : Controller
+{
+	private readonly ILogger<HomeController> _logger;
+
+	public HomeController(ILogger<HomeController> logger)
+	{
+		_logger = logger;
+	}
+
+	[HttpGet]
+	[Route("fachero")]
+	[Route("fachero/{a_texto_prueba}")]
+	public string peliculas(string? a_texto_prueba)
+	{
+		return "holamundo" + a_texto_prueba;
+	}
+
+
+	public IActionResult Index()
+	{
+		return View();
+	}
+
+	public IActionResult Privacy()
+	{
+		return View();
+	}
+
+	[ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+	public IActionResult Error()
+	{
+		return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+	}
+}
---
+++ csharp_asp_practice_2/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace csharp_asp_practice_2.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
