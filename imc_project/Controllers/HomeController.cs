ef4c29bf242644bcc97b5b508943ce51bcb79833
ObamaBolsa <daniel.alegria.lino@gmail.com>
2021-12-17 19:30:21 -0600

    Se añadio el get response de la view IMC del controller Home

---
+++ imc_project/Controllers/HomeController.cs
@@ -0,0 +1,36 @@
+﻿using System.Diagnostics;
+using imc_project.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace imc_project.Controllers;
+
+public class HomeController : Controller
+{
+	private readonly ILogger<HomeController> _logger;
+
+	public HomeController(ILogger<HomeController> logger)
+	{
+		_logger = logger;
+	}
+
+	[HttpGet]
+	public ViewResult Imc() 
+	{
+		return View("Imc_Get");
+	}
+
+	[HttpPost]
+	public ViewResult Imc(string edad, string peso, string estatura) 
+	{
+
+		return View("Imc_Post");
+	}
+
+
+
+	[ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+	public IActionResult Error()
+	{
+		return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+	}
+}
