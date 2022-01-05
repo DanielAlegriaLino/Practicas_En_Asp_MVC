7ffbe1b1eca52ae656fcc58ee57f0fd63ded4adb
ObamaBolsa <daniel.alegria.lino@gmail.com>
2022-01-04 22:35:56 -0600

    Added Redirection to AboutMe view

--- portafolio/Controllers/HomeController.cs
+++ portafolio/Controllers/HomeController.cs
@@ -1,4 +1,5 @@
-﻿using System.Diagnostics;
+﻿using System.Collections;
+using System.Diagnostics;
 using Microsoft.AspNetCore.Mvc;
 using portafolio.Models;
 
@@ -12,6 +13,18 @@ public class HomeController : Controller
     {
         _logger = logger;
     }
+
+  public IActionResult AboutMe() 
+  {
+    ViewBag.nombre = "Daniel" ;
+    ViewBag.apellido = "Lino";
+    ViewBag.ocupacion = "Programador";
+    ViewBag.habilidades_tecnicas = new ArrayList { "HTML","CSS","JS ECMASCRIPT6","C SHARP","JAVA","PYTHON","ASP.NET"};
+
+
+
+    return View();
+  }
 
     public IActionResult Index()
     {
